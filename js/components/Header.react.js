07bc1ee5bfe017597a41ed2a057aec1026bcbb9d
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2015-03-29 13:27:30 +0530

    boostrap header, and react-bootstrap

--- js/components/Header.react.js
+++ js/components/Header.react.js
@@ -1,13 +1,21 @@
 'use strict';
 
 var React = require('react');
+var Navbar = require('react-bootstrap').Navbar;
+var Nav = require('react-bootstrap').Nav;
+var NavItem = require('react-bootstrap').NavItem;
 
 var Header = React.createClass({
 
 	render: function() {
 		return (
 			<div id="header">
-				<span className="brand">thisday</span>
+				<Navbar brand='thisday'>
+					<Nav className="navbar-right">
+						<NavItem eventKey={1} href='#'>Login</NavItem>
+						<NavItem eventKey={1} href='#'>Logout</NavItem>
+					</Nav>
+				</Navbar>
 			</div>
 		);
 	}
