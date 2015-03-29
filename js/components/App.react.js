c059474a58e0b90e0cd072570b573534767499d3
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2015-03-29 13:36:11 +0530

    creating MyGoals view

--- js/components/App.react.js
+++ js/components/App.react.js
@@ -1,68 +1,16 @@
 'use strict';
 
 var React = require('react');
-var GoalsView = require('./GoalsView.react');
-var Dashboard = require('./Dashboard.react');
-var GoalStore = require('../stores/GoalStore');
-var WidgetStore = require('../stores/WidgetStore');
 var Header =  require('./Header.react');
+var MyGoals = require('./MyGoals.react');
 
 var App = React.createClass({
-
-    getInitialState: function () {
-        return {
-            allGoals: [],
-            pointsByDate: []
-        };
-    },
-
-    componentDidMount: function() {
-        GoalStore.addChangeListener(this._onGoalStoreChange);
-        WidgetStore.addChangeListener(this._onWidgetStoreChange);
-        this._updateGoals();
-        this._updateWidgets();
-    },
-
-    componentWillUnmount: function() {
-        GoalStore.removeChangeListener(this._onGoalStoreChange);
-        WidgetStore.removeChangeListener(this._onWidgetStoreChange);
-    },
-
-    _onGoalStoreChange: function() {
-        this._updateGoals();
-        this.setState({
-            completionDate: GoalStore.getCompletionDate()
-        });
-    },
-
-    _onWidgetStoreChange: function () {
-        this._updateWidgets();
-    },
-
-    _updateGoals: function() {
-        GoalStore.getAll(function(goals) {
-            this.setState({
-                allGoals: goals
-            });
-        }.bind(this));
-    },
-
-    _updateWidgets: function () {
-        WidgetStore.getPointsByDate(function (points) {
-            this.setState({
-                pointsByDate: points
-            });
-        }.bind(this));
-    },
 
     render: function() {
         return (
             <div id="container">
                 <Header />
-                <div className="row" id="view">
-                    <GoalsView allGoals={this.state.allGoals} completionDate={this.state.completionDate} />
-                    <Dashboard allGoals={this.state.allGoals} pointsByDate={this.state.pointsByDate} />
-                </div>
+                <MyGoals />
             </div>
         );
     }
---
+++ js/components/MyGoals.react.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var React = require('react');
+var GoalsView = require('./GoalsView.react');
+var Dashboard = require('./Dashboard.react');
+var GoalStore = require('../stores/GoalStore');
+var WidgetStore = require('../stores/WidgetStore');
+
+var MyGoals = React.createClass({
+
+    getInitialState: function() {
+        return {
+            allGoals: [],
+            pointsByDate: []
+        };
+    },
+
+    componentDidMount: function() {
+        GoalStore.addChangeListener(this._onGoalStoreChange);
+        WidgetStore.addChangeListener(this._onWidgetStoreChange);
+        this._updateGoals();
+        this._updateWidgets();
+    },
+
+    componentWillUnmount: function() {
+        GoalStore.removeChangeListener(this._onGoalStoreChange);
+        WidgetStore.removeChangeListener(this._onWidgetStoreChange);
+    },
+
+    _onGoalStoreChange: function() {
+        this._updateGoals();
+        this.setState({
+            completionDate: GoalStore.getCompletionDate()
+        });
+    },
+
+    _onWidgetStoreChange: function() {
+        this._updateWidgets();
+    },
+
+    _updateGoals: function() {
+        GoalStore.getAll(function(goals) {
+            this.setState({
+                allGoals: goals
+            });
+        }.bind(this));
+    },
+
+    _updateWidgets: function () {
+        WidgetStore.getPointsByDate(function (points) {
+            this.setState({
+                pointsByDate: points
+            });
+        }.bind(this));
+    },
+
+    render: function() {
+        return (
+            <div className="row" id="view">
+                <GoalsView allGoals={this.state.allGoals} completionDate={this.state.completionDate} />
+                <Dashboard allGoals={this.state.allGoals} pointsByDate={this.state.pointsByDate} />
+            </div>
+        );
+    }
+
+});
+
+module.exports = MyGoals;
